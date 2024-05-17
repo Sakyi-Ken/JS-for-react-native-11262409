639cc1c19b746d3c7623e01537de92a53239092b
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-05-17 03:05:00 +0000

    updated the js file to include another function

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -5,12 +5,19 @@ function processArray(arrayOfNumbers) {
       return newArray.push(arrayOfNumbers[i] * arrayOfNumbers[i]);
     }
     else {
-      return newArray.push(3 * arrayOfNumbers[i])
+      return newArray.push(3 * arrayOfNumbers[i]);
     }
    }
 }
 
 
-function formatArrayStrings() {
-
+function formatArrayStrings(arrayOfStrings, arrayOfNumbers) {
+  const result = processArray(arrayOfNumbers);
+  for (i = 0; i < arrayOfStrings.length(); i++) {
+    if (result[i] % 2 === 0) {
+      arrayOfStrings[i].capitalize();
+    } else {
+      arrayOfStrings[i].lowercase();
+    }
+  }
 }
